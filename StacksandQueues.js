f9f55831c65da4809123b6cde5f8f3bea025a045
Jerry V <jvr572@gmail.com>
2020-03-29 12:20:32 -0400

    queue done

--- StacksandQueues.js
+++ StacksandQueues.js
@@ -4,7 +4,7 @@ class Node {
     this.next = null;
   }
 }
-
+// LIFO (Last In First Out)
 class Stack {
   constructor(){
     this.first = null;
@@ -41,12 +41,60 @@ class Stack {
   }
 }
 
-const stack = new Stack();
+// const stack = new Stack();
 
-stack.push(1);
-stack.push(2);
-stack.push(3);
-stack.pop();
-stack.pop();
-stack.pop();
-console.log(stack);
+// stack.push(1);
+// stack.push(2);
+// stack.push(3);
+// stack.pop();
+// stack.pop();
+// stack.pop();
+// console.log(stack);
+
+////////////////////////////////////////////// QUEUES///////////////////////////////////
+// FIFO (First In First Out)
+
+class Queue{
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val){
+    let newNode = new Node(val);
+    if(!this.first){
+      this.first = newNode;
+      this.last = newNode;
+    }else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+
+  dequeue(){
+    if(!this.first) return null;
+
+    let rvm = this.first;
+
+    if(this.first === this.last){
+      this.first = null;
+			this.last = null;
+    }else {
+      this.first = this.first.next; 
+    }
+    this.size--;
+    return rvm.value;
+  }
+}
+
+const queue = new Queue();
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+console.log(queue);
