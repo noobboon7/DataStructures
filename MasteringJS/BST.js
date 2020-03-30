1bfcbddf35aabe989de3034d92601e1b0b83bbea
Jerry V <jvr572@gmail.com>
2020-03-30 12:05:54 -0400

    BinarySearchTree insert function

---
+++ MasteringJS/BST.js
@@ -0,0 +1,57 @@
+class Node{
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree{
+  constructor(){
+    this.root = null;
+  }
+
+  insert(val){
+    const newNode = new Node(val);
+
+    if(!this.root){
+      this.root = newNode;
+      return this;
+    }else {
+      let cur = this.root;
+      while(true) {
+        if(val === cur.value) return undefined;
+          if(val < cur.value){
+            if(!cur.left){
+              cur.left = newNode;
+              return this;
+            } 
+            cur = cur.left;
+          } else {
+              if(!cur.right){
+                cur.right = newNode;
+                return this;
+              }
+              cur = cur.right;
+          }
+        }
+    }
+  } 
+
+}
+
+const tree  = new BinarySearchTree();
+tree.insert(10);
+tree.insert(15);
+tree.insert(5);
+tree.insert(16);
+tree.insert(3);
+tree.insert(6);
+// console.log(tree.insert(6))
+// tree.insert(3);
+// tree.root = new Node(10);
+// tree.root.right = new Node(15);
+// tree.root.left = new Node(7);
+// tree.root.left.right = new Node(9);
+
+console.log(tree);
