e9620d8436090e857df65ff461a9ce2577740e43
Jerry V <jvr572@gmail.com>
2020-04-06 20:32:56 -0400

    added edge

--- MasteringJsDS/graph.js
+++ MasteringJsDS/graph.js
@@ -6,11 +6,19 @@ class Graph {
   addVertex(vtx){
     if(!this.adjList[vtx]) this.adjList[vtx] = [];
   }
+
+  addEdge(v1,v2){
+    //add edgecage is v1 || v2 exsists 
+    this.adjList[v1].push(v2);
+    this.adjList[v2].push(v1);
+  }
 }
 const g = new Graph();
 
 g.addVertex("NYC");
-g.addVertex("NYC");
+g.addVertex("Hawaii");
+g.addVertex("Tokyo");
+g.addEdge("NYC", "Hawaii");
 
 console.log(g);
 //////////////////////////////////// NOTES////////////////////////////////////
