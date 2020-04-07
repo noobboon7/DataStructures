59b91b16bdf28378cf976d3e143d671d23f75538
Jerry V <jvr572@gmail.com>
2020-04-06 20:52:06 -0400

    removed Vertex func no edgecase for last algos

--- MasteringJsDS/graph.js
+++ MasteringJsDS/graph.js
@@ -12,6 +12,19 @@ class Graph {
     this.adjList[v1].push(v2);
     this.adjList[v2].push(v1);
   }
+
+  removeEdge(v1,v2){
+    this.adjList[v1] = this.adjList[v1].filter(vtx => vtx !== v2);
+    this.adjList[v2] = this.adjList[v2].filter(vtx => vtx !== v1);
+  }
+
+  removeVertex(vtx){
+    while(this.adjList[vtx].length){
+      let adjVtx = this.adjList[vtx].pop();
+      this.removeEdge(vtx, adjVtx);
+    }
+    delete this.adjList[vtx];
+  }
 }
 const g = new Graph();
 
@@ -19,6 +32,10 @@ g.addVertex("NYC");
 g.addVertex("Hawaii");
 g.addVertex("Tokyo");
 g.addEdge("NYC", "Hawaii");
+g.addEdge("Hawaii", "Tokyo");
+g.addEdge("NYC", "Tokyo");
+// g.removeEdge("NYC", "Tokyo");
+g.removeVertex("Hawaii");
 
 console.log(g);
 //////////////////////////////////// NOTES////////////////////////////////////
