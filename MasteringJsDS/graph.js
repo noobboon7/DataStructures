615ce006b8d662df477b19a9649375c3d3297733
Jerry V <jvr572@gmail.com>
2020-04-08 17:05:30 -0400

    DFS iterative version

--- MasteringJsDS/graph.js
+++ MasteringJsDS/graph.js
@@ -1,49 +1,71 @@
 class Graph {
-  constructor(){
-    this.adjList = {};
-  }
+	constructor() {
+		this.adjList = {};
+	}
 
-  addVertex(vtx){
-    if(!this.adjList[vtx]) this.adjList[vtx] = [];
-  }
+	addVertex(vtx) {
+		if (!this.adjList[vtx]) this.adjList[vtx] = [];
+	}
 
-  addEdge(v1,v2){
-    //add edgecage is v1 || v2 exsists 
-    this.adjList[v1].push(v2);
-    this.adjList[v2].push(v1);
-  }
+	addEdge(v1, v2) {
+		//add edgecage is v1 || v2 exsists
+		this.adjList[v1].push(v2);
+		this.adjList[v2].push(v1);
+	}
 
-  removeEdge(v1,v2){
-    this.adjList[v1] = this.adjList[v1].filter(vtx => vtx !== v2);
-    this.adjList[v2] = this.adjList[v2].filter(vtx => vtx !== v1);
-  }
+	removeEdge(v1, v2) {
+		this.adjList[v1] = this.adjList[v1].filter((vtx) => vtx !== v2);
+		this.adjList[v2] = this.adjList[v2].filter((vtx) => vtx !== v1);
+	}
 
-  removeVertex(vtx){
-    while(this.adjList[vtx].length){
-      let adjVtx = this.adjList[vtx].pop();
-      this.removeEdge(vtx, adjVtx);
-    }
-    delete this.adjList[vtx];
-  }
+	removeVertex(vtx) {
+		while (this.adjList[vtx].length) {
+			let adjVtx = this.adjList[vtx].pop();
+			this.removeEdge(vtx, adjVtx);
+		}
+		delete this.adjList[vtx];
+	}
 
-  DFSRecursive(start){
-    let visited = {},
-        result = [];
-    const adjList = this.adjList;
+	DFSRecursive(start) {
+		let visited = {},
+			result = [];
+		const adjList = this.adjList;
+
+		(function DFS(vertex) {
+			if (!vertex) return null;
+
+			visited[vertex] = true;
+			result.push(vertex);
+
+			adjList[vertex].forEach((neighbor) => {
+				if (!visited[neighbor]) return DFS(neighbor);
+			});
+		})(start);
+
+		return result;
+	}
+
+	DFSIterative(start) {
+		let s = [start],
+    visited = {},
+    results = [],
+    vtx;
     
-    (function DFS(vertex){
-      if(!vertex) return null;
-
-      visited[vertex] = true;
-      result.push(vertex);
+    while (s.length) {
+      vtx = s.pop();
+			visited[vtx] = true;
+      results.push(vtx);
       
-      adjList[vertex].forEach(neighbor => {
-        if(!visited[neighbor]) return DFS(neighbor);
+			this.adjList[vtx].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          s.push(neighbor);
+        }
       });
-    })(start);
-    
-    return result;
+    }
+    return results;
   }
+  
 }
 const g = new Graph();
 
@@ -61,6 +83,7 @@ g.addEdge("D", "E");
 g.addEdge("D", "F");
 g.addEdge("E", "F");
 console.log(g.DFSRecursive("A"));
+console.log(g.DFSIterative("A"));
 // g.addVertex("NYC");
 // g.addVertex("Hawaii");
 // g.addVertex("Tokyo");
@@ -69,7 +92,6 @@ console.log(g.DFSRecursive("A"));
 // g.addEdge("NYC", "Tokyo");
 // g.removeEdge("NYC", "Tokyo");
 // g.removeVertex("Hawaii");
-
 
 console.log(g);
 //////////////////////////////////// NOTES////////////////////////////////////
