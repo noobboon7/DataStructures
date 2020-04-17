e55239c1bdc5196a21aef57f8a2f5b4edf84c6d9
Jerry V <jvr572@gmail.com>
2020-04-17 10:36:26 -0400

    optimized for nearly sorted arrays

--- JsAlgos/BubbleSort.js
+++ JsAlgos/BubbleSort.js
@@ -24,10 +24,30 @@ function es6NaiveBubbleSort(arr){
 
   return arr;
 }
+// optimized for arrays that are nearly sorted 
+function bubbleSort(arr){
+    const swap = (arr, idx1, idx2) => {
+      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    }
+    let noSwaps;
+
+		for (let i = arr.length; i > 0; i--) {
+      noSwaps = true;
+			for (let j = 0; j < i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+          swap(arr, j, j + 1);
+          noSwaps = false;
+				}
+      }
+      if(noSwaps) break;
+		}
+		return arr;
+}
 
 let randArr = [2,6,3,7,8,1,56,456,4,3,87,43];
 
-console.log(naiveBubbleSort(randArr));
+// console.log(naiveBubbleSort(randArr));
 console.log(es6NaiveBubbleSort(randArr));
+console.log(bubbleSort(randArr));
 // not used very often 
 // 
