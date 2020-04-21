a7425415b16e008280dc1afa57b4fe43345a5c00
Jerry V <jvr572@gmail.com>
2020-04-21 13:39:19 -0400

    helper function done

--- JsAlgos/QuickSort.js
+++ JsAlgos/QuickSort.js
@@ -1,5 +1,29 @@
 const quickSort = (arr) => {
   
   
+  
   return arr;
 }
+
+const pivot = (arr, start=0, end=arr.length+1) => {
+  const swap = (arr, i1, i2) => ([arr[i1], arr[i2]] = [arr[i2], arr[i1]]);
+
+  let pvt = arr[start],
+  swapIdx = start;
+
+  for(let i = start + 1; i < arr.length; i++){
+    if(pvt > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i );
+    }
+  }
+  swap(arr, start, swapIdx);
+  console.log(arr);
+  return arr;
+}
+
+
+
+const nArr = [5,32,34,6,21452,2,24,54 ,678,9,7,42234,5424,56,75324,542,52,3,1];
+
+console.log(pivot(nArr));
