0f00271c9b0b07028ee132a6ffffad03dcfef749
Jerry V <jvr572@gmail.com>
2020-04-21 13:58:27 -0400

    done review

--- JsAlgos/QuickSort.js
+++ JsAlgos/QuickSort.js
@@ -1,15 +1,18 @@
-const quickSort = (arr) => {
-  
-  
-  
+const quickSort = (arr, left=0, right=arr.length-1) => {
+  if(left < right){
+    let pvtIdx = pivot(arr, left, right);
+
+    quickSort(arr, left, pvtIdx-1);
+    quickSort(arr, pvtIdx+1, right);
+  }
   return arr;
 }
-
+//helper function 
 const pivot = (arr, start=0, end=arr.length+1) => {
   const swap = (arr, i1, i2) => ([arr[i1], arr[i2]] = [arr[i2], arr[i1]]);
 
   let pvt = arr[start],
-  swapIdx = start;
+      swapIdx = start;
 
   for(let i = start + 1; i < arr.length; i++){
     if(pvt > arr[i]) {
@@ -18,12 +21,13 @@ const pivot = (arr, start=0, end=arr.length+1) => {
     }
   }
   swap(arr, start, swapIdx);
-  console.log(arr);
-  return arr;
+  return swapIdx;
 }
 
 
 
 const nArr = [5,32,34,6,21452,2,24,54 ,678,9,7,42234,5424,56,75324,542,52,3,1];
+const nArr2 = [5,32,56,542,52,3,1];
 
-console.log(pivot(nArr));
+// console.log(pivot(nArr));
+console.log(quickSort(nArr2));
