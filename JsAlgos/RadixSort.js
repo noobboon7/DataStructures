e56e4867c889d3a61fd9267de286f7b5ae7924f1
Jerry V <jvr572@gmail.com>
2020-04-22 13:15:08 -0400

    helper functions for radix sort

---
+++ JsAlgos/RadixSort.js
@@ -0,0 +1,30 @@
+const radixSort = (arr) => {
+  let largestNum = mostDigits(arr)
+
+
+  return arr;
+}
+
+const getDigit = (num, i) => {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+};
+
+const digitCount =(num) => {
+  if(num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+const mostDigits = (nums) => {
+  let maxDigits = 0;
+  for(let num of nums) {
+    maxDigits = Math.max(maxDigits, digitCount(num));
+  }
+  return maxDigits;
+}
+
+const nArr = [5,32,34,6,21452,2,24,54 ,678,9,7,42234,5424,56,75324,542,52,3,1];
+const nArr2 = [5,32,56,542,52,3,1];
+// console.log(radixSort(nArr));
+// console.log(mostDigits(nArr));
+/////////////// NOTES////////////
+// this is not a compare sort algo
