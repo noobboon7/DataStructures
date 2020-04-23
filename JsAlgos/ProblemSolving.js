b069b3608b617e94eedc82fa91b2d81d408225b5
Jerry V <jvr572@gmail.com>
2020-04-23 11:48:30 -0400

    notes

--- JsAlgos/ProblemSolving.js
+++ JsAlgos/ProblemSolving.js
@@ -1,3 +1,24 @@
+////////////////// checklist//////////////
+// 1.understand the problem
+// 2. explore concrete examples 
+// 3. break it down
+// 4. solve/simplify 
+// 5. look back and refactor
+
+// devise a PLAN for the problem and Master common problem solving patterns
+// Restate the problem, what are the inputs, outputs? are answers within input? 
+// how to label important data? 
+
+// test code with edgecases e.g. empty, invaid, simple, complex
+
+// write sudoCode for the code, steps of diagrams 
+
+// implement your sudo code, go the naive route if 
+// you cannot write the more complex version 
+
+// look back at you code and try to optimize it: easy to read, performance (bigO)
+
+////////////////////////////////////////////////////////////////////////////
 // these are some problem solving approaches/patterns 
 
 // Frequnecy pattern using hashs
@@ -30,4 +51,6 @@ const zeroSum = (arr) => {
     else left++;
   }
 }
-console.log(zeroSum([-4,-3,-2,-1,0,1,2,3,10]));
+// console.log(zeroSum([-4,-3,-2,-1,0,1,2,3,10]));
+
+// 
