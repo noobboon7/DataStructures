7f6dbda346a53a714d7c1eee937c1b2c24ccab9b
Jerry Velazquez <Jvr572@gmail.com>
2022-03-17 21:00:37 -0400

    factorial + greet demo

--- Grokking_Algorithm/recursion/factorial.js
+++ Grokking_Algorithm/recursion/factorial.js
@@ -4,3 +4,16 @@ const factorial = (number) => {
 }
 
 console.log(factorial(5));
+
+
+/**
+ * Consider the factorial of the number
+ * @param {number} x Number
+ * @returns {number} Result
+ */
+function fact(x) {
+  if (x === 0) return 1;
+  return x * fact(x - 1);
+}
+
+console.log(fact(5));
