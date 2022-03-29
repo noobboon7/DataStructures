e10c48249ac340a1eb917084c8f21d3479c646eb
Jerry Velazquez <Jvr572@gmail.com>
2022-03-28 21:23:58 -0400

    To be reviewed

--- Grokking_Algorithm/Breadth-first_search/recursiveBFS.js
+++ Grokking_Algorithm/Breadth-first_search/recursiveBFS.js
@@ -12,7 +12,7 @@ graph.thom = [];
  * @param {string} name Friend's name
  * @returns {boolean} Result of checking
  */
-const isSeller = (name) => name[name.length - 1] === "m";
+const isSeller = name => name[name.length - 1] === "m";
 
 /**
  * Find a mango seller
@@ -21,24 +21,24 @@ const isSeller = (name) => name[name.length - 1] === "m";
  * @returns {boolean} Search results
  */
 const search = (name, graph = {}) => {
-	/**
-	 * Recursive function to test people
-	 * @param {Array} waited List of people you need to check
-	 * @param {Set} visited List of checked people
-	 */
-	const iter = (waited = [], visited) => {
-		if (waited.length === 0) return false;
-		const [current, ...rest] = waited;
-		if (visited.has(current)) return iter(rest, visited);
-		if (isSeller(current)) {
-			console.log(`${current} is a mango seller!`);
-			return true;
-		}
-		visited.add(current);
-		const personFriends = graph[current];
-		return iter([...rest, ...personFriends], visited);
-	};
-	return iter(graph[name], new Set());
+  /**
+   * Recursive function to test people
+   * @param {Array} waited List of people you need to check
+   * @param {Set} visited List of checked people
+   */
+  const iter = (waited = [], visited) => {
+    if (waited.length === 0) return false;
+    const [current, ...rest] = waited;
+    if (visited.has(current)) return iter(rest, visited);
+    if (isSeller(current)) {
+      console.log(`${current} is a mango seller!`);
+      return true;
+    }
+    visited.add(current);
+    const personFriends = graph[current];
+    return iter([...rest, ...personFriends], visited);
+  };
+  return iter(graph[name], new Set());
 };
 
 search("you", graph);
